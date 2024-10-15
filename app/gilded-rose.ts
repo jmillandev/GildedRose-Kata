af96063b7b1b9e2599304807b123d62026c5947b
Hector Ortiz Contreras <hectorinho@gmail.com>
2024-10-15 12:14:54 -0600

    Modify by Hector Ortiz

--- app/gilded-rose.ts
+++ app/gilded-rose.ts
@@ -21,7 +21,9 @@ export class GildedRose {
     for (let i = 0; i < this.items.length; i++) {
       if (this.items[i].name != 'Aged Brie' && this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
         if (this.items[i].quality > 0) {
-          if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
+          if (this.items[i].name == 'Conjured') {
+            this.items[i].quality = this.items[i].quality - 2
+          } else if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
             this.items[i].quality = this.items[i].quality - 1
           }
         }
--- test/jest/gilded-rose.spec.ts
+++ test/jest/gilded-rose.spec.ts
@@ -4,6 +4,13 @@ describe('Gilded Rose', () => {
   it('should foo', () => {
     const gildedRose = new GildedRose([new Item('foo', 0, 0)]);
     const items = gildedRose.updateQuality();
-    expect(items[0].name).toBe('fixme');
+    expect(items[0].name).toBe('foo');
+  });
+
+  it('should Conjured', () => {
+    const gildedRose = new GildedRose([new Item('Conjured', 10, 10)]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].name).toBe('Conjured');
+    expect(items[0].quality).toBe(8);
   });
 });
